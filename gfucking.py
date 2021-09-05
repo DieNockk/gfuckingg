a3009e1750d3b1b696534033bfd440aa369870ac
DieNockk <89041764+DieNockk@users.noreply.github.com>
2021-09-06 00:51:29 +0300

    Update gfucking.py

--- gfucking.py
+++ gfucking.py
@@ -1,35 +1,14 @@
-import time
+import pytube
 
-secilen_islem = int(input("""
------------------------------
-         DieNock's
-         made   it
-             ! 
+link = input("""
 
-        1. SQL scanner
-        2. nmap port and script scanner
------------------------------
-"""))
+DieNock's Made it !
 
+indirilecek video,playlist linkini giriniz :
+""")
 
+yt = pytube.YouTube(link)
 
-if secilen_islem == 1:
-    print(" Sql taraması başlatılıyor")
-    time.sleep(3)
-    istek_ip = int(input(" Tarama yapılıcak ip'i giriniz : "))
-    time.sleep(2)
-    print("ip taranıyor....")
-    time.sleep(2)
-    print("Sonuç : ")
-
-
-elif secilen_islem == 2:
-    print("Nmap taraması başlatılıyor")
-    time.sleep(3)
-    nmap_ip = int(input(" Taranıcak ip'i giriniz : "))
-    time.sleep(2)
-    print("İp taranıyor.....")
-    time.sleep(2)
-    print(" Sonuç ")
-
-breakpoint
+stream = yt.streams.first()
+finished = stream.download()
+print("Yükleme tamamlandı")
