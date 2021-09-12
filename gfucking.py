4661ca16d62c368c5994cbf378ca837f4335d940
DieNockk <89041764+DieNockk@users.noreply.github.com>
2021-09-12 15:31:06 +0300

    Update gfucking.py

--- gfucking.py
+++ gfucking.py
@@ -1,14 +1,1 @@
-import pytube
-
-link = input("""
-
-DieNock's Made it !
-
-indirilecek video,playlist linkini giriniz :
-""")
-
-yt = pytube.YouTube(link)
-
-stream = yt.streams.first()
-finished = stream.download()
-print("Yükleme tamamlandı")
+d_soc1ety
